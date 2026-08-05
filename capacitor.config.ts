d4f61f26b35086421181d076ac7632b650ee9b90
bangtut131 <bangtut131@gmail.com>
2026-08-05 18:05:12 +0700

    feat: Enable Auto Live-Update from Vercel in Capacitor Android APK

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'id.mavin.saas',
   appName: 'MAVIN',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'https://mavin-saas.vercel.app',
+    cleartext: true
+  }
 };
 
 export default config;
